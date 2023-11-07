8c64b43a143604dfcd8dcf56268096cb6e974123
Tim Rayner <tim.rayner2020@gmail.com>
2023-11-07 19:50:33 +0000

    feature: added logger to indicate if spotify api is down

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import { Nitro } from "nitropack";
 import mongoose from "mongoose";
+import axios from "axios";
 
 //synchronous function to excecute every time the web application is started; to ensure MongoDB is connected
 export default async (_nitroApp: Nitro) => {
@@ -8,6 +9,19 @@ export default async (_nitroApp: Nitro) => {
     //create mongodb connectionx
     await mongoose.connect(config.mongodbUri);
     console.log("MongoDB Connected");
+
+    const response = await axios
+      .get("https://status.spotify.dev/api/v2/status.json")
+      .then((res) => res.data.status.indicator);
+
+    if (response === "major") {
+      throw createError({
+        statusCode: 503,
+        statusMessage: "Spotify error: Spotify API is down",
+      });
+    } else {
+      console.log("Spotify API is up");
+    }
   } catch (err) {
     console.log(err);
   }
