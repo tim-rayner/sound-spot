e92f16f9999cbfbbf225d3c9ec9b12d606f8ef95
Tim Rayner <tim.rayner2020@gmail.com>
2023-11-08 19:36:43 +0000

    update: added users top lists into profile page

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import axios from "axios";
 import type { iUser } from "~/types/user-types";
 
 const userFS = localStorage.getItem("ud01xy");
@@ -7,8 +8,18 @@ const user = ref<iUser>();
 if (userFS) {
   user.value = JSON.parse(userFS);
 }
+
+const topLists = await axios
+  .get(`/api/users/top/list/${user?.value?._id}`)
+  .then((res) => res.data);
 </script>
 
 <template>
   <ProfileHeader :user="user" />
+  <h3 class="text-xl mx-6">Your Top Lists</h3>
+  <div class="flex flex-row my-12 mt-6 mx-4">
+    <div v-for="list in topLists" :key="list._id" class="h-auto mx-2 w-[40vw]">
+      <ListOverview :list="list" />
+    </div>
+  </div>
 </template>
---
+++ server/api/users/top/list/[id].ts
@@ -0,0 +1,35 @@
+import { useRoute } from "vue-router";
+import { List } from "../../../lists/top.get";
+import { User } from "../../index.get";
+import type { List as iList } from "~/types/list-types";
+
+export default defineEventHandler(async (event): Promise<iList[]> => {
+  const userId = getRouterParam(event, "id");
+
+  console.log("got userId:", userId); //get top lists from database for this user
+  //this will eventually be owned lists, and liked lists (and maybe more)
+  const { spotifyClientAccessToken } = parseCookies(event);
+
+  if (!spotifyClientAccessToken) {
+    // handle error
+    return [];
+  }
+  {
+    owner: userId;
+  }
+  const lists = await List.find({ owner: userId });
+
+  // map lists to replace owner id with owners name from Users collection in each list
+  const newLists = await Promise.all(
+    lists.map(async (list) => {
+      const owner = await User.findById(list.owner);
+      const listWithOwnerName = {
+        ...list.toObject(),
+        owner: owner?.username,
+      };
+      return listWithOwnerName;
+    })
+  );
+
+  return newLists;
+});
